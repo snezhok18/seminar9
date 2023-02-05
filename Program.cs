691172cc0c2911797d30c45fd7ef265855b88d06
Sergey <mma118@yandex.ru>
2023-02-05 11:58:28 +0800

    Added a solution task3

--- Program.cs
+++ Program.cs
@@ -36,32 +36,32 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-int M = new Random().Next(5);
-int N = new Random().Next(7, 15);
-System.Console.WriteLine($"Задан диапазон: от  {M} до {N}.");
-Console.WriteLine($"Сумма элементов = {SumNumbers(M, N)}");
+// int M = new Random().Next(5);
+// int N = new Random().Next(7, 15);
+// System.Console.WriteLine($"Задан диапазон: от  {M} до {N}.");
+// Console.WriteLine($"Сумма элементов = {SumNumbers(M, N)}");
 
-//  --- Method ---
+// //  --- Method ---
 
-int SumNumbers(int M, int N)
-{
-    if (M == 0)
-    {
-        return (N * (N + 1)) / 2;
-    }
-    if (M == N)
-    {
-        return M;
-    }
-    if (M < N)
-    {
-        return N + SumNumbers(M, N - 1);
-    }
-    else
-    {
-        return N + SumNumbers(M, N + 1);
-    }
-}
+// int SumNumbers(int M, int N)
+// {
+//     if (M == 0)
+//     {
+//         return (N * (N + 1)) / 2;
+//     }
+//     if (M == N)
+//     {
+//         return M;
+//     }
+//     if (M < N)
+//     {
+//         return N + SumNumbers(M, N - 1);
+//     }
+//     else
+//     {
+//         return N + SumNumbers(M, N + 1);
+//     }
+// }
 
 
 // Задача 68:
@@ -70,3 +70,23 @@ int SumNumbers(int M, int N)
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+// Console.WriteLine("Введите первое число: ");
+// uint M = uint.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите второе число: ");
+// uint N = uint.Parse(Console.ReadLine()!);
+// Console.WriteLine($"m = {M}, n = {N} -> A(m,n) = {Akk(M, N)}");
+// Console.WriteLine();
+
+// // --- Method ---
+
+// uint Akk(uint n, uint m)
+// {
+//     if (n == 0)
+//         return m + 1;
+//     else
+//       if ((n != 0) && (m == 0))
+//         return Akk(n - 1, 1);
+//     else
+//         return Akk(n - 1, Akk(n, m - 1));
+// }
